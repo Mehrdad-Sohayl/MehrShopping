b07a4a4459e010712e1a1503169b66972eb7d367
Mehrdad <MehSohayl@gmail.com>
2026-05-16 12:37:37 +0330

    feat(application): introduce personal info service interface
    Define IPersonalInfoService and DTO for fetching customer data from an external source, preparing the foundation for the registration command.

---
+++ src/MehrShopping.Application/Interfaces/IPersonalInfoService.cs
@@ -0,0 +1,9 @@
+﻿namespace MehrShopping.Application.Interfaces
+{
+    public interface IPersonalInfoService
+    {
+        Task<PersonalInfoDto> GetAsync(string nationalCode);
+    }
+
+    public record PersonalInfoDto(string FirstName, string LastName, string NationalCode);
+}
